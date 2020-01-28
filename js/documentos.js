9270008ade71ff70f515612736f8a4576a4d942f
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-28 20:49:20 -0300

    Busca da pasta pelo CPF no Firebase Storage

--- js/documentos.js
+++ js/documentos.js
@@ -1,19 +1,27 @@
 function search() {
     var cpf = document.getElementById('inputCPF').value;
-    console.log(cpf);
     validation(cpf);
 }
 
 function validation(cpfValue) {
+    var storage = firebase.storage();
+    // Retorna uma promisses que será processada
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        if (todosArquivos.items.length >= 1) {
+            next(cpfValue);
+        } else {
+            alert('CPF não encontrado');
+        }
+        console.log(todosArquivos.items);
+    }).cath(function(error) {
+        console.log('ERRO', error);        
+    });
+}
+
+function next(cpfValue) {
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificado de: ' + cpfValue;
-
-    var timestamp = new Date().getTime();
-    var database = firebase.database();
-    database.ref(timestamp).set({
-    cpf: inputCPF
-    })
 }
 
 function back() {
@@ -22,15 +30,22 @@ function back() {
     document.getElementById('inputCPF').value = '';
 }
 
-// function enviar() {
-//     var emailValue = document.getElementById('emailTxt').value;
-//     var areaValue = document.getElementById('areaTxt').value;
-//     var timestamp = new Date().getTime();
 
-//     // A variavel database vai receber as funções de acesso ao banco de dados
-//     var database = firebase.database();
-//     database.ref(timestamp).set({
-//         email: emailValue,
-//         mensagem: areaValue
-//     });    
-// }
+    // var timestamp = new Date().getTime();
+    // var database = firebase.database();
+    // database.ref(timestamp).set({
+    // cpf: inputCPF
+    // })
+
+/* function enviar() {
+     var emailValue = document.getElementById('emailTxt').value;
+     var areaValue = document.getElementById('areaTxt').value;
+     var timestamp = new Date().getTime();
+
+     A variavel database vai receber as funções de acesso ao banco de dados
+     var database = firebase.database();
+     database.ref(timestamp).set({
+         email: emailValue,
+         mensagem: areaValue
+     });    
+ }*/
