6f4d8e57a20932f9587a8405aa884900e8c758d1
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-27 21:43:44 -0300

    Conexão com o Firebase

--- js/documentos.js
+++ js/documentos.js
@@ -5,5 +5,32 @@ function search() {
 }
 
 function validation(cpfValue) {
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificado de: ' + cpfValue;
+
+    var timestamp = new Date().getTime();
+    var database = firebase.database();
+    database.ref(timestamp).set({
+    cpf: inputCPF
+    })
 }
+
+function back() {
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('inputCPF').value = '';
+}
+
+// function enviar() {
+//     var emailValue = document.getElementById('emailTxt').value;
+//     var areaValue = document.getElementById('areaTxt').value;
+//     var timestamp = new Date().getTime();
+
+//     // A variavel database vai receber as funções de acesso ao banco de dados
+//     var database = firebase.database();
+//     database.ref(timestamp).set({
+//         email: emailValue,
+//         mensagem: areaValue
+//     });    
+// }
---
+++ js/init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBTAq6rXMSUzZnhkKfvQXyqEphaazpHzYM",
+    authDomain: "documentocpf-bc127.firebaseapp.com",
+    databaseURL: "https://documentocpf-bc127.firebaseio.com",
+    projectId: "documentocpf-bc127",
+    storageBucket: "documentocpf-bc127.appspot.com",
+    messagingSenderId: "728485742135",
+    appId: "1:728485742135:web:f2c5556b2ebff42ed2a1b2",
+    measurementId: "G-HQHCK6LYRV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
